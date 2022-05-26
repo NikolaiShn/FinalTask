5e41300b5bf13614c19b491132cf5e59481572c8
Nikolai <svik@live.com>
2022-05-26 19:53:16 +0300

    db script fixes

--- src/main/resources/db_creation.sql
+++ src/main/resources/db_creation.sql
@@ -7,13 +7,19 @@ name varchar(255) NOT NULL,
 lastName varchar(255) NOT NULL
 );
 
+CREATE TABLE IF NOT EXISTS Roles(
+id int PRIMARY KEY AUTO_INCREMENT,
+role varchar(10) UNIQUE NOT NULL
+);
+
 CREATE TABLE IF NOT EXISTS Users(
 id int PRIMARY KEY AUTO_INCREMENT,
 login varchar(50) UNIQUE NOT NULL,
 password varchar(255) NOT NULL,
-role varchar(50) NOT NULL,
+role_id int NOT NULL,
 profile_id int UNIQUE NOT NULL,
-FOREIGN KEY (profile_id) REFERENCES Profiles(id)
+FOREIGN KEY (profile_id) REFERENCES Profiles(id),
+FOREIGN KEY (role_id) REFERENCES Roles(id)
 );
 
 CREATE TABLE IF NOT EXISTS Lessons(
@@ -30,12 +36,12 @@ name varchar(255) UNIQUE NOT NULL
 
 CREATE TABLE IF NOT EXISTS Themes(
 id int PRIMARY KEY AUTO_INCREMENT,
-themeName varchar(255) UNIQUE NOT NULL
+theme_name varchar(255) UNIQUE NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS Sections(
 id int PRIMARY KEY AUTO_INCREMENT,
-sectionName varchar(255) UNIQUE NOT NULL
+section_name varchar(255) UNIQUE NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS Schedules(
@@ -45,9 +51,9 @@ id int PRIMARY KEY AUTO_INCREMENT
 CREATE TABLE IF NOT EXISTS ProfileKnowledgeDirectories(
 id int PRIMARY KEY AUTO_INCREMENT,
 profile_id int NOT NULL,
-knowledgedirectory_id int NOT NULL,
+knowledge_directory_id int NOT NULL,
 FOREIGN KEY (profile_id) REFERENCES Profiles(id),
-FOREIGN KEY (knowledgedirectory_id) REFERENCES KnowledgeDirectories(id)
+FOREIGN KEY (knowledge_directory_id) REFERENCES KnowledgeDirectories(id)
 );
 
 CREATE TABLE IF NOT EXISTS LessonsReviews(
@@ -59,17 +65,17 @@ FOREIGN KEY (lesson_id) REFERENCES Lessons(id)
 
 CREATE TABLE IF NOT EXISTS KnowledgeDirectoriesThemes(
 id int PRIMARY KEY AUTO_INCREMENT,
-knowledgedirectory_id int NOT NULL,
+knowledge_directory_id int NOT NULL,
 theme_id int NOT NULL,
-FOREIGN KEY (knowledgedirectory_id) REFERENCES KnowledgeDirectories(id),
+FOREIGN KEY (knowledge_directory_id) REFERENCES KnowledgeDirectories(id),
 FOREIGN KEY (theme_id) REFERENCES Themes(id)
 );
 
 CREATE TABLE IF NOT EXISTS KnowledgeDirectoriesSections(
 id int PRIMARY KEY AUTO_INCREMENT,
-knowledgedirectory_id int NOT NULL,
+knowledge_directory_id int NOT NULL,
 section_id int NOT NULL,
-FOREIGN KEY (knowledgedirectory_id) REFERENCES KnowledgeDirectories(id),
+FOREIGN KEY (knowledge_directory_id) REFERENCES KnowledgeDirectories(id),
 FOREIGN KEY (section_id) REFERENCES Sections(id)
 );
 
@@ -111,9 +117,10 @@ date timestamp NOT NULL,
 FOREIGN KEY (schedule_id) REFERENCES Schedules(id)
 );
 
-//Заполнение таблиц
-
-//пароль password у всех юзеров
+/*
+//***Заполнение таблиц***
+// - пароль password у всех юзеров
+*/
 
 INSERT INTO Profiles(name, lastName) VALUES ("ivan", "pupkin");
 INSERT INTO Profiles(name, lastName) VALUES ("vitaly", "pupkin");
@@ -121,51 +128,54 @@ INSERT INTO Profiles(name, lastName) VALUES ("kirill", "pupkin");
 INSERT INTO Profiles(name, lastName) VALUES ("artyom", "pupkin");
 INSERT INTO Profiles(name, lastName) VALUES ("evgeniy", "pupkin");
 
-INSERT INTO Users(login, password, role, profile_id) VALUES ("testuser", "$2a$12$OLhaHMa.GYk.2Bp3OI44S.q3m4JGhEHfdkujm0X1Iu9yhNrdWSkOe", "ROLE_USER", 1);
-INSERT INTO Users(login, password, role, profile_id) VALUES ("admin1", "$2a$12$OLhaHMa.GYk.2Bp3OI44S.q3m4JGhEHfdkujm0X1Iu9yhNrdWSkOe", "ROLE_ADMIN", 2);
-INSERT INTO Users(login, password, role, profile_id) VALUES ("admin2", "$2a$12$OLhaHMa.GYk.2Bp3OI44S.q3m4JGhEHfdkujm0X1Iu9yhNrdWSkOe", "ROLE_ADMIN", 3);
-INSERT INTO Users(login, password, role, profile_id) VALUES ("admin3", "$2a$12$OLhaHMa.GYk.2Bp3OI44S.q3m4JGhEHfdkujm0X1Iu9yhNrdWSkOe", "ROLE_ADMIN", 4);
-INSERT INTO Users(login, password, role, profile_id) VALUES ("admin4", "$2a$12$OLhaHMa.GYk.2Bp3OI44S.q3m4JGhEHfdkujm0X1Iu9yhNrdWSkOe", "ROLE_ADMIN", 5);
+INSERT INTO Roles(role) VALUES ("ROLE_ADMIN");
+INSERT INTO Roles(role) VALUES ("ROLE_USER");
+
+INSERT INTO Users(login, password, role_id, profile_id) VALUES ("testuser", "$2a$12$OLhaHMa.GYk.2Bp3OI44S.q3m4JGhEHfdkujm0X1Iu9yhNrdWSkOe", 2, 1);
+INSERT INTO Users(login, password, role_id, profile_id) VALUES ("admin1", "$2a$12$OLhaHMa.GYk.2Bp3OI44S.q3m4JGhEHfdkujm0X1Iu9yhNrdWSkOe", 1, 2);
+INSERT INTO Users(login, password, role_id, profile_id) VALUES ("admin2", "$2a$12$OLhaHMa.GYk.2Bp3OI44S.q3m4JGhEHfdkujm0X1Iu9yhNrdWSkOe", 1, 3);
+INSERT INTO Users(login, password, role_id, profile_id) VALUES ("admin3", "$2a$12$OLhaHMa.GYk.2Bp3OI44S.q3m4JGhEHfdkujm0X1Iu9yhNrdWSkOe", 1, 4);
+INSERT INTO Users(login, password, role_id, profile_id) VALUES ("admin4", "$2a$12$OLhaHMa.GYk.2Bp3OI44S.q3m4JGhEHfdkujm0X1Iu9yhNrdWSkOe", 1, 5);
 
 INSERT INTO KnowledgeDirectories(name) VALUES ("литература");
 INSERT INTO KnowledgeDirectories(name) VALUES ("математика");
 INSERT INTO KnowledgeDirectories(name) VALUES ("физика");
 
-INSERT INTO Themes(themeName) VALUES ("Булгаков");
-INSERT INTO Themes(themeName) VALUES ("Горький");
-INSERT INTO Themes(themeName) VALUES ("Куприн");
+INSERT INTO Themes(theme_name) VALUES ("Булгаков");
+INSERT INTO Themes(theme_name) VALUES ("Горький");
+INSERT INTO Themes(theme_name) VALUES ("Куприн");
 
-INSERT INTO Themes(themeName) VALUES ("стереометрия");
-INSERT INTO Themes(themeName) VALUES ("планиметрия");
-INSERT INTO Themes(themeName) VALUES ("производные");
+INSERT INTO Themes(theme_name) VALUES ("стереометрия");
+INSERT INTO Themes(theme_name) VALUES ("планиметрия");
+INSERT INTO Themes(theme_name) VALUES ("производные");
 
-INSERT INTO Themes(themeName) VALUES ("электричество");
-INSERT INTO Themes(themeName) VALUES ("движение");
+INSERT INTO Themes(theme_name) VALUES ("электричество");
+INSERT INTO Themes(theme_name) VALUES ("движение");
 
-INSERT INTO Sections(sectionName) VALUES ("механика");
-INSERT INTO Sections(sectionName) VALUES ("русская");
-INSERT INTO Sections(sectionName) VALUES ("греческая");
-INSERT INTO Sections(sectionName) VALUES ("геометрия");
+INSERT INTO Sections(section_name) VALUES ("механика");
+INSERT INTO Sections(section_name) VALUES ("русская");
+INSERT INTO Sections(section_name) VALUES ("греческая");
+INSERT INTO Sections(section_name) VALUES ("геометрия");
 
-INSERT INTO KnowledgeDirectoriesThemes(knowledgedirectory_id, theme_id) VALUES (1, 1);
-INSERT INTO KnowledgeDirectoriesThemes(knowledgedirectory_id, theme_id) VALUES (1, 2);
-INSERT INTO KnowledgeDirectoriesThemes(knowledgedirectory_id, theme_id) VALUES (1, 3);
+INSERT INTO KnowledgeDirectoriesThemes(knowledge_directory_id, theme_id) VALUES (1, 1);
+INSERT INTO KnowledgeDirectoriesThemes(knowledge_directory_id, theme_id) VALUES (1, 2);
+INSERT INTO KnowledgeDirectoriesThemes(knowledge_directory_id, theme_id) VALUES (1, 3);
 
-INSERT INTO KnowledgeDirectoriesThemes(knowledgedirectory_id, theme_id) VALUES (2, 4);
-INSERT INTO KnowledgeDirectoriesThemes(knowledgedirectory_id, theme_id) VALUES (2, 5);
-INSERT INTO KnowledgeDirectoriesThemes(knowledgedirectory_id, theme_id) VALUES (2, 6);
+INSERT INTO KnowledgeDirectoriesThemes(knowledge_directory_id, theme_id) VALUES (2, 4);
+INSERT INTO KnowledgeDirectoriesThemes(knowledge_directory_id, theme_id) VALUES (2, 5);
+INSERT INTO KnowledgeDirectoriesThemes(knowledge_directory_id, theme_id) VALUES (2, 6);
 
-INSERT INTO KnowledgeDirectoriesThemes(knowledgedirectory_id, theme_id) VALUES (3, 7);
-INSERT INTO KnowledgeDirectoriesThemes(knowledgedirectory_id, theme_id) VALUES (3, 8);
+INSERT INTO KnowledgeDirectoriesThemes(knowledge_directory_id, theme_id) VALUES (3, 7);
+INSERT INTO KnowledgeDirectoriesThemes(knowledge_directory_id, theme_id) VALUES (3, 8);
 
-INSERT INTO KnowledgeDirectoriesSections(knowledgedirectory_id, section_id) VALUES (1, 2);
-INSERT INTO KnowledgeDirectoriesSections(knowledgedirectory_id, section_id) VALUES (1, 3);
-INSERT INTO KnowledgeDirectoriesSections(knowledgedirectory_id, section_id) VALUES (2, 4);
-INSERT INTO KnowledgeDirectoriesSections(knowledgedirectory_id, section_id) VALUES (3, 1);
+INSERT INTO KnowledgeDirectoriesSections(knowledge_directory_id, section_id) VALUES (1, 2);
+INSERT INTO KnowledgeDirectoriesSections(knowledge_directory_id, section_id) VALUES (1, 3);
+INSERT INTO KnowledgeDirectoriesSections(knowledge_directory_id, section_id) VALUES (2, 4);
+INSERT INTO KnowledgeDirectoriesSections(knowledge_directory_id, section_id) VALUES (3, 1);
 
-INSERT INTO ProfileKnowledgeDirectories(profile_id, knowledgedirectory_id) VALUES (1, 1);
-INSERT INTO ProfileKnowledgeDirectories(profile_id, knowledgedirectory_id) VALUES (2, 2);
-INSERT INTO ProfileKnowledgeDirectories(profile_id, knowledgedirectory_id) VALUES (3, 3);
+INSERT INTO ProfileKnowledgeDirectories(profile_id, knowledge_directory_id) VALUES (1, 1);
+INSERT INTO ProfileKnowledgeDirectories(profile_id, knowledge_directory_id) VALUES (2, 2);
+INSERT INTO ProfileKnowledgeDirectories(profile_id, knowledge_directory_id) VALUES (3, 3);
 
 INSERT INTO Lessons(name, description, cost) VALUES ("урок1", "датадата", 10.0);
 INSERT INTO Lessons(name, description, cost) VALUES ("урок2", "датадата", 11.0);
@@ -230,12 +240,3 @@ INSERT INTO ProfileCourses(profile_id, course_id) VALUES (1, 1);
 INSERT INTO ProfileCourses(profile_id, course_id) VALUES (2, 2);
 INSERT INTO ProfileCourses(profile_id, course_id) VALUES (3, 1);
 INSERT INTO ProfileCourses(profile_id, course_id) VALUES (4, 3);
-
-
-
-
-
-
-
-
-
